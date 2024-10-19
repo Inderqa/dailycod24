54e390c3e8187adbb6e9ec337d06e5462320331f
Inderqa <inder093@gmail.com>
2024-10-19 07:11:14 +0530

    string methods explained

--- str_methd.py
+++ str_methd.py
@@ -17,41 +17,27 @@ txt1 = "My name is {fname}, I'm {age}".format(fname = "John", age = 36)
 print(txt1)
 print(str.strip("a")) # remove leading and trailing whitespace #
 
-# index()	Searches the string for a specified value and returns the position of where it was found
-# isalnum()	Returns True if all characters in the string are alphanumeric
-# isalpha()	Returns True if all characters in the string are in the alphabet
-# isascii()	Returns True if all characters in the string are ascii characters
-# isdecimal()	Returns True if all characters in the string are decimals
-# isdigit()	Returns True if all characters in the string are digits
-# isidentifier()	Returns True if the string is an identifier
-# islower()	Returns True if all characters in the string are lower case
-# isnumeric()	Returns True if all characters in the string are numeric
-# isprintable()	Returns True if all characters in the string are printable
-# isspace()	Returns True if all characters in the string are whitespaces
-# istitle()	Returns True if the string follows the rules of a title
-# isupper()	Returns True if all characters in the string are upper case
-# join()	Converts the elements of an iterable into a string
-# ljust()	Returns a left justified version of the string
-# lower()	Converts a string into lower case
-# lstrip()	Returns a left trim version of the string
-# maketrans()	Returns a translation table to be used in translations
-# partition()	Returns a tuple where the string is parted into three parts
-# replace()	Returns a string where a specified value is replaced with a specified value
-# rfind()	Searches the string for a specified value and returns the last position of where it was found
-# rindex()	Searches the string for a specified value and returns the last position of where it was found
-# rjust()	Returns a right justified version of the string
-# rpartition()	Returns a tuple where the string is parted into three parts
-# rsplit()	Splits the string at the specified separator, and returns a list
-# rstrip()	Returns a right trim version of the string
-# split()	Splits the string at the specified separator, and returns a list
-# splitlines()	Splits the string at line breaks and returns a list
-# startswith()	Returns true if the string starts with the specified value
-# strip()	Returns a trimmed version of the string
-# swapcase()	Swaps cases, lower case becomes upper case and vice versa
-# title()	Converts the first character of each word to upper case
-# translate()	Returns a translated string
-# upper()	Converts a string into upper case
-# zfill()	Fills the string with a specified number of 0 values at the beginning
-#
+# 1. strings are immutable.
+# 2. any method on string takes a copy of the object.
+# 3.string.upper will convert the string to an upper case
+# 4.string.lower will convert the string to lower case
+# 5. string.rstrip("character") will strip the trailing characters of string
+# 6.string.replace("character/alphabets", new character/alphabets") will replace the existing specified alphabets in string with new ones
+# 7. string.split splits the string at the given alphabet and returns a list of items
+# 8.string.capitalize capitalizes the first character of the word and turns rest all the characters to lower case
+# 9.string.center center aligns the string by adding the number of spaces mentioned in parenthesis
+# 10. string.count counts the total number of a particular set of characters in a string
+# 11. string.endswith checks whether a string ends with the specific characters
+# 12.string.find finds the first occurrence of a given value and return the index value of the position of that occurrence
+# 13.string.index finds the occurrence of a given value and returns the index value of the position however, if it is unable to find it will give an error causing the program to exit
+# 14.string.isalnum checks to find if string is alphanumeric and returns true or false
+# 15.string.isalpha checks if there are any numbers in the string
+# 16.string.islower checks if there are only lower aphabets in the string
+# 17. string.isprintable checks if all the characters are printable in the string(non printable characters e.g \n)
+# 18 string.isspace checks if any space bar has been used in the string
+# 19.string.istitle The istitile() returns True only if the first letter of each word of the string is capitalized, else 20. string.isupper checks if all characters are uppercase in a string
+# 21. string.startswith checks if a string starts with a given value
+# 22. string.swapcase convert uppercase to lowercase and vice versa in a string
+# 23.string.title converts first character of all the words in the sentence to capital
 
 
